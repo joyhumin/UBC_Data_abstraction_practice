c15ed74572ee4fbae3d4f63dc5094d481316911b
Nerdsexy <nerdissexy@gmail.com>
2020-03-29 16:45:23 +1300

    Add class specification

--- src/model/Transcript.java
+++ src/model/Transcript.java
@@ -1,4 +1,22 @@
 package model;
 
 public class Transcript {
+    //TODO: Design specification for this method
+    //REQUIRES: grade >= 0, course != NULL
+    //MODIFIES: this
+    //EFFECTS: add course to this course
+    public void addGrade(String course, double grade){ }
+
+    //TODO: Design specification for this method
+    // This method should return course name and grade in some consistent String format
+    //EFFECTS: return course name and grade
+    public String getCourseAndGrade(String course){ return null; }
+
+    //TODO: Design specification for this method
+    //EFFECTS: print out the transcript
+    public void printTranscript(){ }
+
+    //TODO: Design specification for this method
+    //EFFECTS: calculate and return GPA
+    public double getGPA(){ return 0.0; }
 }
