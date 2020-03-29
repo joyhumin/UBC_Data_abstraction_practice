98c38067cdc89f0757ec91872594472afc95c211
Nerdsexy <nerdissexy@gmail.com>
2020-03-29 16:50:58 +1300

    Add IntegerSet class

---
+++ src/model/IntergerSet.java
@@ -0,0 +1,17 @@
+package model;
+
+public class IntergerSet {
+
+    //MODIFIES: this
+    //EFFECTS: insert the integer into the set unless it's already there, in which case do nothing
+    public void insert (Integer num) {
+
+    }
+
+    //MODIFIES: this
+    // EFFECTS: if the integer is in the set, then remove it from the set;
+    // Otherwise, do nothing.
+    public void remove (Integer num){
+
+    }
+}
